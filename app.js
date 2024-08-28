079c41500e2a0ef16a2c72fc695fa437f1c22ed3
ArudoPE <aortizparodi@outlook.com>
2024-08-27 21:04:22 -0500

    Adding more functions, making encription possible!

--- app.js
+++ app.js
@@ -6,5 +6,42 @@ function clearText(){
 }
 */
 
+function showResponseContainer(){
+    document.getElementById("main__right__container__yes__answer").style.display = "flex";
+    hideNoAnswerContainer();
+}
+
+function hideResponseContainer(){
+    document.getElementById("main__right__container__yes__answer").style.display = "none";
+}
+
+function showNoAnswerContainer(){
+    document.getElementById("main__right__container__no__answer").style.display = "flex";
+}
+
+function hideNoAnswerContainer(){
+    document.getElementById("main__right__container__no__answer").style.display = "none";
+}
+
 
 /*Encriptador*/
+function encriptar(){
+    /*Capturing the input*/
+    let userInput = document.getElementById('input-area').value;
+
+    /*Encription Process*/
+    console.log("encriptar");
+    console.log(userInput);
+    console.log(typeof(userInput));
+    showResponseContainer();
+}
+
+function desencriptar(){
+    console.log("desencriptar");
+}
+
+function copiar(){
+    console.log("copiar")
+}
+
+
